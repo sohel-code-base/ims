01b4b692edc603db51f0208a4eca125616699f62
soheldeveloper <sohelrananayanpur@gmail.com>
2021-02-09 00:26:02 +0600

    Added login page

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_login")
+     */
+    public function login(): Response
+    {
+        return $this->render('pages/login.html.twig');
+    }
+
+    /**
+     * @return Response
+     * @Route("/dashboard", name="app_dashboard")
+     */
+    public function index()
+    {
+        return $this->render('pages/dashboard.html.twig');
+    }
+}
