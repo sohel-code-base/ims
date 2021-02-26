ef709db7ac6b14c5401743f3ed3d6a8582908c9a
soheldeveloper <sohelrananayanpur@gmail.com>
2021-02-26 20:20:52 +0600

    Added product edit feature

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -41,5 +41,55 @@ class DashboardController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/{id}/edit", name="product_edit")
+     * @param $id
+     * @param Request $request
+     * @param ProductRepository $repository
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function editProduct($id, Request $request, ProductRepository $repository)
+    {
+        $product = $repository->find($id);
+
+        $form = $this->createForm(ProductFormType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+            $this->addFlash('success', 'Product updated successfully!');
+
+            return $this->redirectToRoute('app_index');
+
+        }
+        return $this->render('product/edit.html.twig',[
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @param $id
+     * @param ProductRepository $repository
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route("/{id}/delete", name="product_delete")
+     */
+    public  function deleteProduct($id, ProductRepository $repository)
+    {
+        $product = $repository->find($id);
+        if ($product){
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($product);
+            $em->flush();
+            $this->addFlash('success', 'Product deleted successfully!');
+
+            return $this->redirectToRoute('app_index');
+        }else{
+            $this->addFlash('error', 'Product not found!');
+            return $this->redirectToRoute('app_index');
+        }
+    }
+
 
 }
