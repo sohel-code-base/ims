b075580da2eb308a6a8fa358a2408e34d9bcdbd9
soheldeveloper <sohelrananayanpur@gmail.com>
2021-06-01 22:32:34 +0600

    Product Info edit and delete feature added

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\ProductType;
+use App\Repository\ProductPurchaseRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,14 +12,19 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
+/**
+ * Class ProductController
+ * @package App\Controller
+ * @Route("/product")
+ */
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/product/info", name="product_info")
+     * @Route("/list", name="product_info")
      * @param ProductRepository $repository
      * @return Response
      */
-    public function infoProduct(ProductRepository $repository): Response
+    public function itemInfo(ProductRepository $repository): Response
     {
         $productInfo = $repository->findAll();
         return $this->render('product/index.html.twig',[
@@ -27,11 +33,11 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/product/new", methods={"GET","POST"}, name="new_product")
+     * @Route("/new", methods={"POST"}, name="new_product")
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
      */
-    public function addProduct(Request $request)
+    public function newItem(Request $request)
     {
         $product = new Product();
         $form = $this->createForm(ProductType::class, $product);
@@ -50,5 +56,58 @@ class ProductController extends AbstractController
         return $this->render('product/addProduct.html.twig',[
             'form' => $form->createView(),
         ]);
+    }
+    /**
+     * @Route("/{id}/edit", methods={"GET","POST"}, name="edit_product")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function editItem(Request $request, $id, ProductRepository $repository)
+    {
+        $findItem = $repository->find($id);
+
+        $form = $this->createForm(ProductType::class, $findItem)->remove('createdAt');
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()){
+            $findItem->setUpdatedAt(new \DateTime('now'));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($findItem);
+            $em->flush();
+            $this->addFlash('update', 'Product details updated!');
+            return $this->redirectToRoute('product_info');
+        }
+
+        return $this->render('product/editProduct.html.twig',[
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @param $id
+     * @param ProductRepository $productRepository
+     * @Route("/{id}/delete", name="delete_product")
+     */
+    public function deleteItem($id, ProductRepository $productRepository, ProductPurchaseRepository $purchaseRepository)
+    {
+        $findItem = $productRepository->find($id);
+        if ($findItem){
+            $findPurchase = $purchaseRepository->findBy(['product' => $findItem]);
+            if (!$findPurchase){
+                $em = $this->getDoctrine()->getManager();
+                $em->remove($findItem);
+                $em->flush();
+                $this->addFlash('success', 'Item has been deleted!');
+                return $this->redirectToRoute('product_info');
+
+            } else {
+                $this->addFlash('error', 'Stored product items can not be deleted!');
+                return $this->redirectToRoute('product_info');
+            }
+        } else {
+            $this->addFlash('error', 'Item not found!');
+            return $this->redirectToRoute('product_info');
+        }
     }
 }
